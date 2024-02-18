29b06675d2503207309a266f705eb2af5f6561c9
Dev Chauhan <devccode8@gmail.com>
2024-02-18 23:11:24 +0530

    comming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,87 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl text-white font-bold">Comming Soon</h1>
-    </main>
+    <Layout>
+      <img
+        src="/images/xera.svg"
+        alt="Themeptation "
+        className="absolute h-96 -top-20 -right-16 lg:right-5 lg:top-10 animate-blob"
+      />
+      <img
+        src="/images/shapes.svg"
+        alt="hero"
+        className="absolute w-full left-24 bottom-24 animate-blob2"
+      />
+      <div className="w-full flex items-center justify-center gap-10">
+        <img src="/images/csi_logo.png" alt="hero" width={100} />
+        <img src="/images/MIET_Logo.webp" alt="hero" width={120} />
+      </div>
+      <div className="relative z-10 py-6 space-y-16 lg:space-y-32 text-gray-900 ">
+        <div className="text-center space-y-10">
+          <h3 className="text-3xl font-semibold uppercase tracking-wider">
+            Coming soon
+          </h3>
+          <h1 className="text-7xl lg:text-9xl font-extrabold">
+            We’r blowing up
+          </h1>
+          <p className="text-xl lg:text-2xl tracking-wide mx-10 lg:max-w-xl lg:mx-auto">
+            We`re under construction. Check back for an update soon. Stay in
+            touch!
+          </p>
+        </div>
+      </div>
+      <div className="absolute bottom-5">
+        <ul className="flex space-x-3">
+          <li>
+            <Link href="https://www.instagram.com/csi_miet/">
+              <div className="flex items-center justify-center w-10 h-10 text-gray-900 rounded-full bg-gray-50 transition  ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
+                <svg
+                  className="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://www.youtube.com/@csi_miet/">
+              <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 transition ease-in-out duration-500 text-gray-700 hover:bg-red-500 hover:text-white hover:shadow-lg">
+                <svg
+                  className="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path d="M16.23 7.102c-2.002-.136-6.462-.135-8.461 0-2.165.148-2.419 1.456-2.436 4.898.017 3.436.27 4.75 2.437 4.898 1.999.135 6.459.136 8.461 0 2.165-.148 2.42-1.457 2.437-4.898-.018-3.436-.271-4.75-2.438-4.898zm-6.23 7.12v-4.444l4.778 2.218-4.778 2.226zm2-12.222c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z" />
+                </svg>
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link href="https://www.linkedin.com/company/computer-society-of-india-miet/mycompany/">
+              <div className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 text-gray-700 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
+                <svg
+                  className="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                </svg>
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </Layout>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,48 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    backgroundImage: {
+      hero:
+        "url('https://html.creativegigstf.com/vCamp/vCamp/images/assets/bg_01.png')",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        blob: "blob 7s infinite",
+        blob2: "blob2 10s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+            filter: "blur(2px)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+            filter: "blur(0px)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+            filter: "blur(2px)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+            filter: "blur(1px)",
+          },
+        },
+        blob2: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+            filter: "blur(2px)",
+          },
+          "50%": {
+            transform: "translate(20px, -20px) scale(1.1)",
+            filter: "blur(0px)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+            filter: "blur(1px)",
+          },
+        },
       },
     },
   },
